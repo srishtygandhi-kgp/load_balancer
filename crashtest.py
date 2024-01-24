c2d070c1d0e632950afa4b249d90085d2dae8a48
Yatindra Indoria <yatindraindoria75@gmail.com>
2024-01-24 22:01:21 +0530

    added the crash and respawn analysis

---
+++ crashtest.py
@@ -0,0 +1,49 @@
+import requests
+import concurrent.futures
+import os
+import json
+import sys
+
+# Analysis for the crash, and respawn
+
+def validInt(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+def cmd(n):
+    hostnames = ["S" + str(i+1) for i in range(n)]
+    curl_command = 'curl -X POST -H "Content-Type: application/json" -d \'{{"n": {}, "hostnames": {}}}\' http://localhost:5000/add'.format(n, json.dumps(hostnames))
+    return curl_command
+
+def send_request(url):
+    try:
+        response = requests.get(url)
+        return response.json()["message"]
+    except Exception as e:
+        return str(e)
+
+def main():
+    base_url = "http://localhost:5000/home"
+    num_requests = 10000
+    data = {'Hit': 0, 'Miss' : 0}
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        urls = [base_url for _ in range(num_requests)]
+        responses = list(executor.map(send_request, urls))
+
+    # with open("server_responses.txt", "w") as file:
+    for response in responses:
+        server = response.split()[-1]
+        if validInt(server):
+            data['Hit'] += 1
+        else:
+            data['Miss'] += 1
+            # file.write(response + "\n")
+    print(data['Hit'] / num_requests)
+
+if __name__ == "__main__":
+    os.system(cmd(int(sys.argv[1])))
+    main()
