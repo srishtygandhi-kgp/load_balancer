cd8d497368e578eb56415b224ccabf4dd5b45b25
Yatindra Indoria <yatindraindoria75@gmail.com>
2024-01-24 21:48:29 +0530

    changing main to read response

--- analysis.py
+++ analysis.py
@@ -11,14 +11,20 @@ def send_request(url):
 def main():
     base_url = "http://localhost:5000/home"
     num_requests = 10000
+    data = {}
 
     with concurrent.futures.ThreadPoolExecutor() as executor:
         urls = [base_url for _ in range(num_requests)]
         responses = list(executor.map(send_request, urls))
 
-    with open("server_responses.txt", "w") as file:
-        for response in responses:
-            file.write(response + "\n")
+    # with open("server_responses.txt", "w") as file:
+    for response in responses:
+        server = response.split()[-1]
+        if server in data:
+            data[server] += 1
+        else:
+            data[server] = 1
+            # file.write(response + "\n")
 
 if __name__ == "__main__":
     main()
