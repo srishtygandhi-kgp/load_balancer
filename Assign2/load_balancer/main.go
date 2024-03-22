55cb77026fcf9ca7bc5076916af23bb38973020d
r-avenous <atulyaankitsharma@kgpian.iitkgp.ac.in>
2024-03-22 22:45:12 +0530

    lb

--- Assign2/load_balancer/main.go
+++ Assign2/load_balancer/main.go
@@ -179,110 +179,208 @@ func removeContainer(server string) error {
 	return nil
 }
 
-func main() {
+type configPayload struct {
+	Schema schema   `json:"schema" binding:"required"`
+	Shards []string `json:"shards" binding:"required"`
+}
 
-	g_server_shards_mapping = make(map[string]map[string]bool)
-	g_shard_servers_mapping = make(map[string]map[string]bool)
-	g_server_port_mapping = make(map[string]int)
-	g_port_server_mapping = make(map[int]string)
-	g_shards = make(map[string]shardMetaData)
-	g_shard_current_idx = make(map[string]int)
+type initPayload struct {
+	N       int
+	Schema  schema
+	Shards  []shard
+	Servers map[string][]string
+}
 
-	for i := 0; i < M; i++ {
-		g_servers_hashmap[i] = ""
-	}
-	fmt.Printf("Proxy starting...")
-
-	r := gin.Default()
-
-	r.POST("/init", initHandler)
-	r.GET("/status", statusHandler)
-	r.POST("/add", addHandler)
-	r.DELETE("/rm", rmHandler)
-	r.POST("/read", readHandler)
-	r.POST("/write", writeHandler)
-	r.PUT("/update", updateHandler)
-	r.DELETE("/del", delHandler)
-
-	//check heartbeat and respwan if needed
-	s := gocron.NewScheduler(time.UTC)
-	_, err := s.Every(5).Seconds().SingletonMode().Do(checkHeartbeat)
+func initHandler(c *gin.Context) {
+	jsonData := getJSONstring(c)
+	//fmt.Printf("\n%v", jsonData)
+	var payload initPayload
+	err := json.Unmarshal([]byte(jsonData), &payload)
 	if err != nil {
+		fmt.Println("Error decoding JSON:", err)
+		c.JSON(http.StatusBadRequest, gin.H{"message": "Error decoding JSON", "status": "failure"})
 		return
 	}
-	s.StartAsync()
-
-	port := "5000"
-	err = r.Run(":" + port)
-	if err != nil {
-		log.Fatalf("Error starting server: %v", err)
+	g_schema = payload.Schema
+	for _, shard := range payload.Shards {
+		g_shards[shard.Shard_id] = shardMetaData{shard.Stud_id_low, shard.Stud_id_low + shard.Shard_size, &sync.Mutex{}}
 	}
-}
-
-func delHandler(c *gin.Context) {
-
-}
-
-func updateHandler(c *gin.Context) {
-
-}
-
-func writeHandler(c *gin.Context) {
-	var payload struct {
-		Data []student
+	//fmt.Printf("\n%v", payload.N)
+	//fmt.Printf("\n%v", payload.Schema)
+	//fmt.Printf("\n%v", payload.Shards)
+	//fmt.Printf("\n%v\n", payload.Servers)
+	for server := range payload.Servers {
+		err := spawnContainer(server)
+		if err != nil {
+			log.Printf("%v", err)
+			continue
+		}
 	}
+	for server, shards := range payload.Servers {
+		var body configPayload
+		body.Schema = g_schema
+		body.Shards = shards
+		jsonBody, err := json.Marshal(body)
+		configEndpoint := fmt.Sprintf("http://%s:5000/config", server)
+		post, err := http.Post(configEndpoint, "application/json", bytes.NewReader(jsonBody))
+		for {
+			if err == nil {
+				break
+			}
+			post, err = http.Post(configEndpoint, "application/json", bytes.NewReader(jsonBody))
+		}
+		//if err != nil {
+		//	//TODO: Handle post request failure
+		//	log.Printf("%v", err)
+		//	continue
+		//}
+		//if status not success print
+		if post.StatusCode != http.StatusOK {
+			//TODO: print error
+		}
+		for _, shard_ := range shards {
+			if _, ok := g_shard_servers_mapping[shard_]; !ok {
+				g_shard_servers_mapping[shard_] = make(map[string]bool)
+			}
+			g_shard_servers_mapping[shard_][server] = true
+			if _, ok := g_server_shards_mapping[server]; !ok {
+				g_server_shards_mapping[server] = make(map[string]bool)
+			}
+			g_server_shards_mapping[server][shard_] = true
+		}
+	}
+
+	// return OK
+	c.JSON(http.StatusOK, gin.H{"message": "Configured Database", "status": "success"})
+}
+
+func statusHandler(c *gin.Context) {
+	var shardlist []shard
+	for shard_ := range g_shards {
+		shardlist = append(shardlist, shard{g_shards[shard_].Stud_id_low, shard_, g_shards[shard_].Stud_id_high - g_shards[shard_].Stud_id_low})
+	}
+	server_shards_mapping := make(map[string][]string)
+	for server, shards := range g_server_shards_mapping {
+		for shard_ := range shards {
+			server_shards_mapping[server] = append(server_shards_mapping[server], shard_)
+		}
+	}
+	c.JSON(http.StatusOK, gin.H{"N": g_server_count, "schema": g_schema, "shards": shardlist, "servers": server_shards_mapping})
+}
+
+func addHandler(c *gin.Context) {
 	jsonString := getJSONstring(c)
+	var payload addPayload
 	err := json.Unmarshal([]byte(jsonString), &payload)
 	if err != nil {
 		fmt.Println("Error decoding JSON:", err)
 		c.JSON(http.StatusBadRequest, gin.H{"message": "Error decoding JSON", "status": "failure"})
 		return
 	}
-	dataToWriteToShards := make(map[string][]student)
-	for _, row := range payload.Data {
-		for shard_ := range g_shards {
-			if row.Stud_id >= g_shards[shard_].Stud_id_low && row.Stud_id < g_shards[shard_].Stud_id_high {
-				dataToWriteToShards[shard_] = append(dataToWriteToShards[shard_], row)
+
+	for _, shard_ := range payload.New_shards {
+		g_shards[shard_.Shard_id] = shardMetaData{shard_.Stud_id_low, shard_.Stud_id_low + shard_.Shard_size, &sync.Mutex{}}
+	}
+
+	for serverGiven, shards := range payload.Servers {
+		server := ""
+		for _, c := range serverGiven {
+			if (c >= '0' && c <= '9') || (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') {
+				server += string(c)
+			}
+		}
+		err := spawnContainer(server)
+		if err != nil {
+			log.Printf("%v", err)
+			continue
+		}
+		var body configPayload
+		body.Schema = g_schema
+		body.Shards = shards
+		jsonBody, err := json.Marshal(body)
+		configEndpoint := fmt.Sprintf("http://%s:5000/config", server)
+		post, err := http.Post(configEndpoint, "application/json", bytes.NewReader(jsonBody))
+		for {
+			if err == nil {
 				break
 			}
+			post, err = http.Post(configEndpoint, "application/json", bytes.NewReader(jsonBody))
+		}
+		//if err != nil {
+		//	//TODO: Handle post request failure
+		//	log.Printf("%v", err)
+		//	continue
+		//}
+		//if status not success print
+		if post.StatusCode != http.StatusOK {
+			//TODO: print error
+			continue
+		}
+		for _, shard_ := range shards {
+			if _, ok := g_shard_servers_mapping[shard_]; !ok {
+				g_shard_servers_mapping[shard_] = make(map[string]bool)
+			}
+			g_shard_servers_mapping[shard_][server] = true
+			if _, ok := g_server_shards_mapping[server]; !ok {
+				g_server_shards_mapping[server] = make(map[string]bool)
+			}
+			g_server_shards_mapping[server][shard_] = true
 		}
 	}
-	for shard_, rows := range dataToWriteToShards {
-		fmt.Printf("\nFor data %v\n", rows)
-		done := true
-		for server := range g_shard_servers_mapping[shard_] {
-			fmt.Printf("\nForwarding to %s\n", server)
-			writeEndpoint := fmt.Sprintf("http://%s:5000/write", server)
-			var body struct {
-				Shard    string
-				Curr_idx int
-				Data     []student
-			}
-			body.Shard = shard_
-			// check if shard_ is in g_shard_current_idx
-			if _, ok := g_shard_current_idx[shard_]; !ok {
-				body.Curr_idx = g_shard_current_idx[shard_]
-			} else {
-				body.Curr_idx = 0
-				g_shard_current_idx[shard_] = 0
-			}
-			body.Data = rows
-			jsonBody, _ := json.Marshal(body)
-			// lock mutex for this shard
-			g_shards[shard_].Mutex.Lock()
-			res, err := http.Post(writeEndpoint, "application/json", bytes.NewReader(jsonBody))
-			g_shards[shard_].Mutex.Unlock()
-			if err != nil {
-				fmt.Printf("\n%v\n%v", err, res)
-				done = false
-				continue
-			}
-		}
-		if done {
-			g_shard_current_idx[shard_] += len(rows)
-		}
+
+	var toSend struct {
+		N       int
+		Message string
+		Status  string
 	}
-	c.JSON(http.StatusOK, gin.H{"message": strconv.Itoa(len(payload.Data)) + " Data entries added", "status": "success"})
+	toSend.N = g_server_count
+	toSend.Message = "Added"
+	for serv := range payload.Servers {
+		toSend.Message += " " + serv
+	}
+	toSend.Status = "success"
+	// return OK
+	c.JSON(http.StatusOK, toSend)
+}
+
+func rmHandler(c *gin.Context) {
+	jsonString := getJSONstring(c)
+	var payload struct {
+		N       int
+		Servers []string
+	}
+	err := json.Unmarshal([]byte(jsonString), &payload)
+	if err != nil {
+		fmt.Println("Error decoding JSON:", err)
+		c.JSON(http.StatusBadRequest, gin.H{"message": "Error decoding JSON", "status": "failure"})
+		return
+	}
+	// sanity check
+	if len(payload.Servers) > payload.N {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "<Error> Length of server list is more than removable instances", "status": "failure"})
+		return
+	}
+	// add servers to the list if not enough
+	for server := range g_server_shards_mapping {
+		if len(payload.Servers) == payload.N {
+			break
+		}
+		payload.Servers = append(payload.Servers, server)
+	}
+	// remove servers from the list
+	for _, server := range payload.Servers {
+		err := removeContainer(server)
+		if err != nil {
+			log.Printf("%v", err)
+			continue
+		}
+		// remove server from shard mapping
+		for shard_ := range g_server_shards_mapping[server] {
+			delete(g_shard_servers_mapping[shard_], server)
+		}
+		delete(g_server_shards_mapping, server)
+	}
+	c.JSON(http.StatusOK, gin.H{"message": gin.H{"N": g_server_count, "servers": payload.Servers}, "status": "success"})
 }
 
 type readPayload struct {
@@ -346,101 +444,210 @@ func readHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"shards_queried": response.shards_queried, "data": response.Data, "status": response.Status})
 }
 
-func rmHandler(c *gin.Context) {
-
-}
-
-func addHandler(c *gin.Context) {
+func writeHandler(c *gin.Context) {
+	var payload struct {
+		Data []student
+	}
 	jsonString := getJSONstring(c)
-	var payload addPayload
 	err := json.Unmarshal([]byte(jsonString), &payload)
 	if err != nil {
 		fmt.Println("Error decoding JSON:", err)
 		c.JSON(http.StatusBadRequest, gin.H{"message": "Error decoding JSON", "status": "failure"})
 		return
 	}
-
-	for _, shard_ := range payload.New_shards {
-		g_shards[shard_.Shard_id] = shardMetaData{shard_.Stud_id_low, shard_.Stud_id_low + shard_.Shard_size, &sync.Mutex{}}
-	}
-
-	for serverGiven, shards := range payload.Servers {
-		server := ""
-		for _, c := range serverGiven {
-			if (c >= '0' && c <= '9') || (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') {
-				server += string(c)
-			}
-		}
-		err := spawnContainer(server)
-		if err != nil {
-			log.Printf("%v", err)
-			continue
-		}
-		var body configPayload
-		body.Schema = g_schema
-		body.Shards = shards
-		jsonBody, err := json.Marshal(body)
-		configEndpoint := fmt.Sprintf("http://%s:5000/config", server)
-		post, err := http.Post(configEndpoint, "application/json", bytes.NewReader(jsonBody))
-		for {
-			if err == nil {
+	dataToWriteToShards := make(map[string][]student)
+	for _, row := range payload.Data {
+		for shard_ := range g_shards {
+			if row.Stud_id >= g_shards[shard_].Stud_id_low && row.Stud_id < g_shards[shard_].Stud_id_high {
+				dataToWriteToShards[shard_] = append(dataToWriteToShards[shard_], row)
 				break
 			}
-			post, err = http.Post(configEndpoint, "application/json", bytes.NewReader(jsonBody))
 		}
-		if post.StatusCode != http.StatusOK {
-			//TODO: print error
-			continue
-		}
-		for _, shard_ := range shards {
-			if _, ok := g_shard_servers_mapping[shard_]; !ok {
-				g_shard_servers_mapping[shard_] = make(map[string]bool)
+	}
+	for shard_, rows := range dataToWriteToShards {
+		fmt.Printf("\nFor data %v\n", rows)
+		done := true
+		for server := range g_shard_servers_mapping[shard_] {
+			fmt.Printf("\nForwarding to %s\n", server)
+			writeEndpoint := fmt.Sprintf("http://%s:5000/write", server)
+			var body struct {
+				Shard    string
+				Curr_idx int
+				Data     []student
 			}
-			g_shard_servers_mapping[shard_][server] = true
-			if _, ok := g_server_shards_mapping[server]; !ok {
-				g_server_shards_mapping[server] = make(map[string]bool)
+			body.Shard = shard_
+			// check if shard_ is in g_shard_current_idx
+			if _, ok := g_shard_current_idx[shard_]; !ok {
+				body.Curr_idx = g_shard_current_idx[shard_]
+			} else {
+				body.Curr_idx = 0
+				g_shard_current_idx[shard_] = 0
 			}
-			g_server_shards_mapping[server][shard_] = true
+			body.Data = rows
+			jsonBody, _ := json.Marshal(body)
+			// lock mutex for this shard
+			g_shards[shard_].Mutex.Lock()
+			res, err := http.Post(writeEndpoint, "application/json", bytes.NewReader(jsonBody))
+			g_shards[shard_].Mutex.Unlock()
+			if err != nil {
+				fmt.Printf("\n%v\n%v", err, res)
+				done = false
+				continue
+			}
+		}
+		if done {
+			g_shard_current_idx[shard_] += len(rows)
 		}
 	}
-
-	var toSend struct {
-		N       int
-		Message string
-		Status  string
-	}
-	toSend.N = g_server_count
-	toSend.Message = "Added"
-	for serv := range payload.Servers {
-		toSend.Message += " " + serv
-	}
-	toSend.Status = "success"
-	// return OK
-	c.JSON(http.StatusOK, toSend)
+	c.JSON(http.StatusOK, gin.H{"message": strconv.Itoa(len(payload.Data)) + " Data entries added", "status": "success"})
 }
 
-func statusHandler(c *gin.Context) {
-	var shardlist []shard
+type updatePayload struct {
+	Shard   string  `json:"shard" binding:"required"`
+	Stud_id int     `json:"Stud_id" binding:"required"`
+	Data    student `json:"data" binding:"required"`
+}
+
+func updateHandler(c *gin.Context) {
+	var payload struct {
+		Stud_id int
+		Data    student
+	}
+	jsonString := getJSONstring(c)
+	err := json.Unmarshal([]byte(jsonString), &payload)
+	if err != nil {
+		fmt.Println("Error decoding JSON:", err)
+		c.JSON(http.StatusBadRequest, gin.H{"message": "Error decoding JSON", "status": "failure"})
+		return
+	}
 	for shard_ := range g_shards {
-		shardlist = append(shardlist, shard{g_shards[shard_].Stud_id_low, shard_, g_shards[shard_].Stud_id_high - g_shards[shard_].Stud_id_low})
-	}
-	server_shards_mapping := make(map[string][]string)
-	for server, shards := range g_server_shards_mapping {
-		for shard_ := range shards {
-			server_shards_mapping[server] = append(server_shards_mapping[server], shard_)
+		if payload.Stud_id >= g_shards[shard_].Stud_id_low && payload.Stud_id < g_shards[shard_].Stud_id_high {
+			for server := range g_shard_servers_mapping[shard_] {
+				fmt.Printf("\nForwarding to %s\n", server)
+				updateEndpoint := fmt.Sprintf("http://%s:5000/update", server)
+				var body updatePayload
+				body.Data = payload.Data
+				body.Stud_id = payload.Stud_id
+				body.Shard = shard_
+				jsonBody, err := json.Marshal(body)
+				if err != nil {
+					//TODO: handle json error
+				}
+				// mutex for this shard
+				g_shards[shard_].Mutex.Lock()
+				put, err := http.NewRequest(http.MethodPut, updateEndpoint, bytes.NewReader(jsonBody))
+				g_shards[shard_].Mutex.Unlock()
+				if err != nil {
+					//TODO: handle request error
+					continue
+				}
+				client := http.Client{}
+				g_shards[shard_].Mutex.Lock()
+				do, err := client.Do(put)
+				g_shards[shard_].Mutex.Unlock()
+				if err != nil {
+					//TODO: handle put error
+					continue
+				}
+				if do.StatusCode != http.StatusOK {
+					//TODO: handle update failed
+					continue
+				}
+			}
 		}
 	}
-	c.JSON(http.StatusOK, gin.H{"N": g_server_count, "schema": g_schema, "shards": shardlist, "servers": server_shards_mapping})
-
+	c.JSON(http.StatusOK, gin.H{"message": "Data entry for Stud_id: " + strconv.Itoa(payload.Stud_id) + " updated", "status": "success"})
 }
 
-type configPayload struct {
-	Schema schema   `json:"schema" binding:"required"`
-	Shards []string `json:"shards" binding:"required"`
+type delPayload struct {
+	Shard   string `json:"shard" binding:"required"`
+	Stud_id int    `json:"Stud_id" binding:"required"`
 }
 
-func initHandler(c *gin.Context) {
+func delHandler(c *gin.Context) {
+	var payload struct {
+		Stud_id int
+	}
+	jsonString := getJSONstring(c)
+	err := json.Unmarshal([]byte(jsonString), &payload)
+	if err != nil {
+		fmt.Println("Error decoding JSON:", err)
+		c.JSON(http.StatusBadRequest, gin.H{"message": "Error decoding JSON", "status": "failure"})
+		return
+	}
+	for shard_ := range g_shards {
+		if payload.Stud_id >= g_shards[shard_].Stud_id_low && payload.Stud_id < g_shards[shard_].Stud_id_high {
+			for server := range g_shard_servers_mapping[shard_] {
+				fmt.Printf("\nForwarding to %s\n", server)
+				delEndpoint := fmt.Sprintf("http://%s:5000/del", server)
+				var body delPayload
+				body.Shard = shard_
+				body.Stud_id = payload.Stud_id
+				jsonBody, _ := json.Marshal(body)
+				g_shards[shard_].Mutex.Lock()
+				del, err := http.NewRequest(http.MethodDelete, delEndpoint, bytes.NewReader(jsonBody))
+				g_shards[shard_].Mutex.Unlock()
+				if err != nil {
+					//TODO: handle request error
+					continue
+				}
+				client := http.Client{}
+				g_shards[shard_].Mutex.Lock()
+				do, err := client.Do(del)
+				g_shards[shard_].Mutex.Unlock()
+				if err != nil {
+					//TODO: handle del error
+					continue
+				}
+				if do.StatusCode != http.StatusOK {
+					//TODO: handle delete failed
+					continue
+				}
+			}
+			break
+		}
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "Data entry for Stud_id: " + strconv.Itoa(payload.Stud_id) + " removed from all replicas", "status": "success"})
+}
 
+func main() {
+
+	g_server_shards_mapping = make(map[string]map[string]bool)
+	g_shard_servers_mapping = make(map[string]map[string]bool)
+	g_server_port_mapping = make(map[string]int)
+	g_port_server_mapping = make(map[int]string)
+	g_shards = make(map[string]shardMetaData)
+	g_shard_current_idx = make(map[string]int)
+
+	for i := 0; i < M; i++ {
+		g_servers_hashmap[i] = ""
+	}
+	fmt.Printf("Proxy starting...")
+
+	r := gin.Default()
+
+	r.POST("/init", initHandler)
+	r.GET("/status", statusHandler)
+	r.POST("/add", addHandler)
+	r.DELETE("/rm", rmHandler)
+	r.POST("/read", readHandler)
+	r.POST("/write", writeHandler)
+	r.PUT("/update", updateHandler)
+	r.DELETE("/del", delHandler)
+
+	//check heartbeat and respwan if needed
+	s := gocron.NewScheduler(time.UTC)
+	_, err := s.Every(5).Seconds().SingletonMode().Do(checkHeartbeat)
+	if err != nil {
+		return
+	}
+	s.StartAsync()
+
+	port := "5000"
+	err = r.Run(":" + port)
+	if err != nil {
+		log.Fatalf("Error starting server: %v", err)
+	}
 }
 
 func checkHeartbeat() {
@@ -468,6 +675,7 @@ func checkHeartbeat() {
 			log.Default().Printf("Error spawning new server container for %s: %v", server, err)
 			continue
 		}
+		//make map keyset to array
 		var shards []string
 		for sh := range failure_server_shard_mapping[server] {
 			shards = append(shards, sh)
@@ -484,7 +692,14 @@ func checkHeartbeat() {
 			}
 			post, err = http.Post(configEndpoint, "application/json", bytes.NewReader(jsonBody))
 		}
+		//if err != nil {
+		//	//TODO: Handle post request failure
+		//	log.Printf("%v", err)
+		//	continue
+		//}
+		//if status not success print
 		if post.StatusCode != http.StatusOK {
+			//TODO: print error
 			log.Default().Printf("Error configuring server %s: %v", server, err)
 			continue
 		}
@@ -506,10 +721,12 @@ func checkHeartbeat() {
 				if err != nil {
 					log.Default().Printf("Error creating copy request for %s: %v", server, err)
 					continue
+					//TODO: handle request error
 				}
 				client := http.Client{}
 				do, err := client.Do(copyReq)
 				if err != nil {
+					//TODO: handle copy failed
 					log.Default().Printf("Error copying data to %s: %v", server, err)
 					continue
 				}
