09b9724e4e30e54d7696dd7a67fc06d6327ea2fe
Yatindra Indoria <yatindraindoria75@gmail.com>
2024-04-14 22:45:13 +0530

    accessory file -- types.go

---
+++ Assign3/shard_manager /types.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"io/ioutil"
+	"log"
+	"sync"
+)
+
+type shard struct {
+	Stud_id_low int
+	Shard_id    string
+	Shard_size  int
+}
+
+type shardMetaData struct {
+	Stud_id_low  int
+	Stud_id_high int
+	Mutex        *sync.Mutex
+}
+
+type addPayload struct {
+	N          int
+	New_shards []shard
+	Servers    map[string][]string
+}
+
+type student struct {
+	Stud_id    int
+	Stud_name  string
+	Stud_marks int
+}
+
+type configPayload struct {
+	Shards   []string                   `json:"shards" binding:"required"`
+	Map_data map[string]map[string]bool `json:"map_data" binding:"required"`
+}
+
+type initPayload struct {
+	N       int
+	Shards  []shard
+	Servers map[string][]string
+}
+type readPayload struct {
+	Shard   string         `json:"shard" binding:"required"`
+	Stud_id map[string]int `json:"Stud_id" binding:"required"`
+}
+
+type readResponse struct {
+	Data   []student
+	Status string
+}
+
+func getJSONstring(c *gin.Context) string {
+	body, err := ioutil.ReadAll(c.Request.Body)
+	if err != nil {
+		log.Println("Error reading request body:", err)
+		return "}"
+	}
+	return string(body)
+}
