037f6e2c13cde5b78c90dd386f129bb53929ce73
Yatindra Indoria <yatindraindoria75@gmail.com>
2024-04-14 22:47:01 +0530

    check_heartbeat

--- Assign3/shard_manager /main.go
+++ Assign3/shard_manager /main.go
@@ -9,6 +9,7 @@ import (
 	"log"
 	"net/http"
 	"os/exec"
+	"sync"
 	"time"
 )
 
@@ -16,6 +17,7 @@ var (
 	g_shard_server_map = make(map[string]map[string]bool)
 	g_port_mapping     = make(map[string]int)
 	g_port_counter     = 5010
+	infoLock           = &sync.Mutex{}
 )
 
 func getServerPortMapping(server string) int {
@@ -28,6 +30,8 @@ func getServerPortMapping(server string) int {
 }
 
 func psinfoHandler(c *gin.Context) {
+	infoLock.Lock()
+	defer infoLock.Unlock()
 	c.JSON(http.StatusOK, g_shard_server_map)
 }
 
@@ -225,4 +229,145 @@ func rmHandler(c *gin.Context) {
 	}
 	broadcastShardServerMap()
 	c.JSON(http.StatusOK, gin.H{"message": "Removed servers", "status": "success"})
+}
+
+func reElect(shard string) {
+	mostUpdated := ""
+	longestLog := -1
+	for server := range g_shard_server_map[shard] {
+		// get log length
+		jsonValue, _ := json.Marshal(gin.H{"shard": shard})
+		res, err := http.Post(fmt.Sprintf("http://%s:5000/lenlog", server), "application/json", bytes.NewBuffer(jsonValue))
+		if err != nil {
+			log.Printf("Error getting log length from %s: %v", server, err)
+			continue
+		}
+		// read response
+		var resStruct struct {
+			Length int
+		}
+		err = json.NewDecoder(res.Body).Decode(&resStruct)
+		if err != nil {
+			log.Printf("Error decoding response from %s: %v", server, err)
+			continue
+		}
+		if resStruct.Length > longestLog {
+			longestLog = resStruct.Length
+			mostUpdated = server
+		}
+	}
+	for server := range g_shard_server_map[shard] {
+		g_shard_server_map[shard][server] = false
+	}
+	fmt.Printf("\n%s elected as primary for shard %s\n", mostUpdated, shard)
+	g_shard_server_map[shard][mostUpdated] = true
+}
+
+func main() {
+
+	r := gin.Default()
+
+	r.POST("/init", initHandler)
+	r.POST("/add", addHandler)
+	r.POST("/rm", rmHandler)
+	r.GET("/psinfo", psinfoHandler)
+
+	//check heartbeat and respawn if needed
+	s := gocron.NewScheduler(time.UTC)
+	_, err := s.Every(5).Seconds().SingletonMode().Do(checkHeartbeat)
+	if err != nil {
+		return
+	}
+	s.StartAsync()
+
+	port := "5000"
+	err = r.Run(":" + port)
+	if err != nil {
+		log.Fatalf("Error starting Load Balancer: %v", err)
+	}
+}
+
+func checkHeartbeat() {
+	failure_server_shard_mapping := make(map[string]map[string]bool)
+	server_shard_mapping := make(map[string]map[string]bool)
+	for shard_, s := range g_shard_server_map {
+		for server := range s {
+			if server_shard_mapping[server] == nil {
+				server_shard_mapping[server] = make(map[string]bool)
+			}
+			server_shard_mapping[server][shard_] = true
+		}
+	}
+	// get heartbeat from all servers and store unresponsive servers
+	for server, shards := range server_shard_mapping {
+		_, err := http.Get(fmt.Sprintf("http://%s:5000/heartbeat", server))
+		if err != nil {
+			log.Printf("Error getting heartbeat from %s: %v", server, err)
+			failure_server_shard_mapping[server] = make(map[string]bool)
+			for shard_ := range shards {
+				failure_server_shard_mapping[server][shard_] = true
+			}
+		}
+	}
+	if len(failure_server_shard_mapping) == 0 {
+		return
+	}
+	infoLock.Lock()
+	defer infoLock.Unlock()
+	for server, _ := range failure_server_shard_mapping {
+		// remove container
+		err := removeContainer(server)
+		if err != nil {
+			log.Printf("%v", err)
+			continue
+		}
+		// remove server from g_shard_server_map
+		for shard_ := range failure_server_shard_mapping[server] {
+			delete(g_shard_server_map[shard_], server)
+		}
+	}
+	for shard_, s := range g_shard_server_map {
+		reElectRequired := true
+		for server := range s {
+			if s[server] == true {
+				reElectRequired = false
+				break
+			}
+		}
+		if reElectRequired {
+			reElect(shard_)
+		}
+	}
+	broadcastShardServerMap()
+	// spawn new containers for failed servers
+	for server, shards := range failure_server_shard_mapping {
+		err := spawnContainer(server)
+		if err != nil {
+			log.Printf("%v", err)
+			continue
+		}
+		var body configPayload
+		body.Shards = make([]string, 0)
+		for shard_ := range shards {
+			body.Shards = append(body.Shards, shard_)
+		}
+		body.Map_data = g_shard_server_map
+		jsonBody, err := json.Marshal(body)
+		configEndpoint := fmt.Sprintf("http://%s:5000/config", server)
+		post, err := http.Post(configEndpoint, "application/json", bytes.NewReader(jsonBody))
+		for {
+			if err == nil {
+				break
+			}
+			post, err = http.Post(configEndpoint, "application/json", bytes.NewReader(jsonBody))
+		}
+		if post.StatusCode != http.StatusOK {
+			fmt.Printf("Error configuring server %s: %v", server, err)
+			continue
+		}
+		for _, shard_ := range body.Shards {
+			g_shard_server_map[shard_][server] = false
+		}
+	}
+	broadcastShardServerMap()
 }
